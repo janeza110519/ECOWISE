d807f46ce92c18f5378325183f61c98dbc07c5ab
Beth17-dev <aprilbethsalapang30@gmail.com>
2026-05-08 15:30:21 +0800

    editing the html

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "connection.php";
+include "connect.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-include "connection.php";
+include "connect.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
